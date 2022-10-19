3acab0868ba3e6c5d6f0c14b66ab135b88841acd
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2022-10-19 18:04:33 +1300

    Fix DuckDB

--- duckdb/main.py
+++ duckdb/main.py
@@ -4,18 +4,18 @@ import time
 import duckdb
 
 queries = [
-    "select * from %s",
-    "select * from %s where service = 'frontend'",
-    "select * from %s where service = 'frontend' and host = 'i-1ec3d9e2506434b2.ec2.internal'",
-    "select * from %s where service = 'frontend' and host = 'i-1ec3d9e2506434b2.ec2.internal' and time > '1970-01-01 00:00:00.008000'::timestamp",
-    "select request_duration_ns from %s",
-    "select request_duration_ns from %s where service = 'frontend'",
-    "select request_duration_ns from %s where service = 'frontend' and host = 'i-1ec3d9e2506434b2.ec2.internal'",
-    "select request_duration_ns from %s where service = 'frontend' and host = 'i-1ec3d9e2506434b2.ec2.internal' and time > '1970-01-01 00:00:00.008000'::timestamp",
-    "select client_addr from %s",
-    "select client_addr from %s where service = 'frontend'",
-    "select client_addr from %s where service = 'frontend' and host = 'i-1ec3d9e2506434b2.ec2.internal'",
-    "select client_addr from %s where service = 'frontend' and host = 'i-1ec3d9e2506434b2.ec2.internal' and time > '1970-01-01 00:00:00.008000'::timestamp",
+    "select * from '%s'",
+    "select * from '%s' where service = 'frontend'",
+    "select * from '%s' where service = 'frontend' and host = 'i-1ec3d9e2506434b2.ec2.internal'",
+    "select * from '%s' where service = 'frontend' and host = 'i-1ec3d9e2506434b2.ec2.internal' and time > '1970-01-01 00:00:00.008000'::timestamp",
+    "select request_duration_ns from '%s'",
+    "select request_duration_ns from '%s' where service = 'frontend'",
+    "select request_duration_ns from '%s' where service = 'frontend' and host = 'i-1ec3d9e2506434b2.ec2.internal'",
+    "select request_duration_ns from '%s' where service = 'frontend' and host = 'i-1ec3d9e2506434b2.ec2.internal' and time > '1970-01-01 00:00:00.008000'::timestamp",
+    "select client_addr from '%s'",
+    "select client_addr from '%s' where service = 'frontend'",
+    "select client_addr from '%s' where service = 'frontend' and host = 'i-1ec3d9e2506434b2.ec2.internal'",
+    "select client_addr from '%s' where service = 'frontend' and host = 'i-1ec3d9e2506434b2.ec2.internal' and time > '1970-01-01 00:00:00.008000'::timestamp",
 ]
 
 file = os.getenv("FILE")
